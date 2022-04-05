ec803e7b7c079e7d8d0e74630b5124bcd1ddd09b
toby petty <tobypetty@hotmail.com>
2022-04-04 21:01:45 -0400

    fix command

--- lrs2_2_landmark_roi.sh
+++ lrs2_2_landmark_roi.sh
@@ -4,8 +4,8 @@ lrs2=/home/ubuntu/w251-final-project/lrs2/untarred/mvlrs_v1
 ffmpeg=/usr/bin/ffmpeg
 
 python3 /home/ubuntu/av_hubert/avhubert/preparation/detect_landmark.py \
- --root ${lrs2}
- --landmark ${lrs2}/landmark
+ --root ${lrs2} \
+ --landmark ${lrs2}/landmark \
  --manifest ${lrs2}/file.list \
  --cnn_detector /home/ubuntu/av_hubert/avhubert/preparation/cnn_face_detector.py \
  --face_detector /home/ubuntu/av_hubert/avhubert/preparation/face_landmark_detection.py \
