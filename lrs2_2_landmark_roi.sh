1ba770fbbddff4c77faad2289cac2e615d7d0854
toby petty <tobypetty@hotmail.com>
2022-04-04 22:42:46 -0400

    update model filepaths

--- lrs2_2_landmark_roi.sh
+++ lrs2_2_landmark_roi.sh
@@ -7,7 +7,7 @@ python3 /home/ubuntu/av_hubert/avhubert/preparation/detect_landmark.py \
  --root ${lrs2} \
  --landmark ${lrs2}/landmark \
  --manifest ${lrs2}/file.list \
- --cnn_detector /home/ubuntu/av_hubert/avhubert/preparation/cnn_face_detector.py \
- --face_predictor /home/ubuntu/av_hubert/avhubert/preparation/face_detector.py \
+ --cnn_detector /home/ubuntu/w251-final-project/lrs2/dlib_models/mmod_human_face_detector.dat \
+ --face_predictor /home/ubuntu/w251-final-project/lrs2/dlib_models/shape_predictor_68_face_landmarks.dat \
  --ffmpeg ${ffmpeg} \
  --rank 0 --nshard 1
